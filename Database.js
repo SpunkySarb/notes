0f135e216bcf3d69f88da4acbdb06ab66c896607
sarbs <sarbsarbsagar143@gmail.com>
2022-04-29 09:29:02 -0400

    g

--- Database.js
+++ Database.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('notes', 'root', '1212', { host: 'localhost', dialect: 'mysql' });
+const sequelize = new Sequelize('r9u2ph3refi5olrx', 'kw0fnfic2vhchumw', 'ltuydeq91zpovjxw', { host: 'bv2rebwf6zzsv341.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', dialect: 'mysql' });
 
 module.exports = sequelize;
